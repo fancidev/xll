e1a25ad35653993962de8e86c237890fe350f1e4
fancidev <fancidev@gmail.com>
2015-08-22 22:23:26 +0800

    Refactor UDF wrapper code.

--- XllAddinHelper/XllAddin.h
+++ XllAddinHelper/XllAddin.h
@@ -174,52 +174,56 @@ public:
 template <typename T> struct fake_dependency : public std::false_type {};
 
 template <typename T>
-struct UdfArgument
+struct ArgumentWrapper
 {
-	static_assert(fake_dependency<T>::value, 
-		"The supplied type is not supported as a udf argument. "
-		"Specialize UdfArgument<T> to support it.");
-	typedef T boxed_type;
-	static inline T unbox(T value) { return value; }
+	static_assert(fake_dependency<T>::value,
+		"Do not know how to wrap the supplied argument type. "
+		"Specialize xll::ArgumentWrapper<T> to support it.");
+	// typedef ?? wrapped_type;
+	// static inline T unwrap(wrapped_type value);
 };
 
-template <typename T, typename TBoxed = T>
-struct UdfArgumentImpl
-{
-	typedef TBoxed boxed_type;
-	static inline T unbox(TBoxed v) { return v; }
-};
+#define DEFINE_SIMPLE_ARGUMENT_WRAPPER(NativeType, WrappedType) \
+	template <> struct ArgumentWrapper<NativeType> \
+	{ \
+		typedef WrappedType wrapped_type; \
+		static inline NativeType unwrap(WrappedType v) { return v; } \
+	}
 
-#define MARSHAL_UDF_ARGUMENT(NativeType, BoxedType) \
-	template <> struct UdfArgument<NativeType> : UdfArgumentImpl<NativeType, BoxedType> {}
+DEFINE_SIMPLE_ARGUMENT_WRAPPER(int, int);
+DEFINE_SIMPLE_ARGUMENT_WRAPPER(double, double);
+DEFINE_SIMPLE_ARGUMENT_WRAPPER(std::wstring, const wchar_t *);
 
-MARSHAL_UDF_ARGUMENT(int,          int);
-MARSHAL_UDF_ARGUMENT(double,       double);
-MARSHAL_UDF_ARGUMENT(std::wstring, const wchar_t *);
+template <typename T> struct ArgumentWrapper<T &> : ArgumentWrapper < T > {};
+template <typename T> struct ArgumentWrapper<T &&> : ArgumentWrapper < T > {};
+template <typename T> struct ArgumentWrapper<T const> : ArgumentWrapper < T > {};
+template <typename T> struct ArgumentWrapper<T volatile> : ArgumentWrapper < T > {};
+template <typename T> struct ArgumentWrapper<T const volatile> : ArgumentWrapper < T > {};
 
 template <typename T>
-struct UdfReturnValue
+struct ReturnValueWrapper
 {
-	static_assert(fake_dependency<T>::value, "The supplied return value is not supported.");
-	static_assert(fake_dependency<T>::value, "Specialize UdfReturnValue<T> to support it.");
-	typedef T boxed_type;
-	static boxed_type box(const T &);
+	static_assert(fake_dependency<T>::value,
+		"Do not know how to wrap the supplied return value type. "
+		"Specialize xll::ReturnValueWrapper<T> to support it.");
+	// typedef ?? wrapped_type;
+	// static inline wrapped_type wrap(const T &);
 };
 
-#define IMPLEMENT_SIMPLE_RETURN_VALUE(type) \
-	template <> struct UdfReturnValue<type> \
+#define DEFINE_SIMPLE_RETURN_VALUE_WRAPPER(NativeType) \
+	template <> struct ReturnValueWrapper<NativeType> \
 	{ \
-		typedef type boxed_type; \
-		static inline type box(const type &v) { return v; } \
+		typedef NativeType wrapped_type; \
+		static inline wrapped_type wrap(const NativeType &v) { return v; } \
 	};
 
-IMPLEMENT_SIMPLE_RETURN_VALUE(int);
-IMPLEMENT_SIMPLE_RETURN_VALUE(double);
+DEFINE_SIMPLE_RETURN_VALUE_WRAPPER(int);
+DEFINE_SIMPLE_RETURN_VALUE_WRAPPER(double);
 
-template <> struct UdfReturnValue < std::wstring >
+template <> struct ReturnValueWrapper < std::wstring >
 {
-	typedef LPXLOPER12 boxed_type;
-	static inline LPXLOPER12 box(const std::wstring &s)
+	typedef LPXLOPER12 wrapped_type;
+	static inline LPXLOPER12 wrap(const std::wstring &s)
 	{
 		LPXLOPER12 op = (LPXLOPER12)malloc(sizeof(XLOPER12));
 		if (op == NULL)
@@ -238,6 +242,10 @@ template <> struct UdfReturnValue < std::wstring >
 		// todo: set the xlDLLFree bit
 	}
 };
+
+template <typename T> struct ReturnValueWrapper<T const> : ReturnValueWrapper < T >{};
+template <typename T> struct ReturnValueWrapper<T volatile> : ReturnValueWrapper < T >{};
+template <typename T> struct ReturnValueWrapper<T const volatile> : ReturnValueWrapper < T >{};
 
 #pragma endregion
 
@@ -277,28 +285,27 @@ template <typename T> struct ArgumentTypeText
 
 #define GET_ARGUMENT_TYPE_TEXT(type) ArgumentTypeText<type>::getTypeText()
 #else
-template <typename T> inline const wchar_t * getTypeText()
+template <typename T> inline const wchar_t * GetTypeText()
 {
-	static_assert(false, "Does not support marshalling of the supplied type.");
+	static_assert(false, "The supplied type is not a valid XLL argument type.");
 }
-#define DECLARE_ARGUMENT_TYPE_TEXT(type, text) \
-	template<> inline const wchar_t * getTypeText<type>() { return L##text; }
-#define GET_ARGUMENT_TYPE_TEXT(type) getTypeText<type>()
+#define DEFINE_TYPE_TEXT(type, text) \
+	template<> inline const wchar_t * GetTypeText<type>() { return L##text; }
 #endif
 
-DECLARE_ARGUMENT_TYPE_TEXT(bool,       "A");
-DECLARE_ARGUMENT_TYPE_TEXT(bool*,      "L");
-DECLARE_ARGUMENT_TYPE_TEXT(double,     "B");
-DECLARE_ARGUMENT_TYPE_TEXT(double*,    "E");
-DECLARE_ARGUMENT_TYPE_TEXT(char*,      "C");
-DECLARE_ARGUMENT_TYPE_TEXT(uint16_t,   "H");
-DECLARE_ARGUMENT_TYPE_TEXT(int16_t,    "I");
-DECLARE_ARGUMENT_TYPE_TEXT(int16_t*,   "M");
-DECLARE_ARGUMENT_TYPE_TEXT(int32_t,    "J");
-DECLARE_ARGUMENT_TYPE_TEXT(int32_t*,   "N");
-DECLARE_ARGUMENT_TYPE_TEXT(wchar_t*,   "C%");
-DECLARE_ARGUMENT_TYPE_TEXT(LPCWSTR,    "C%");
-DECLARE_ARGUMENT_TYPE_TEXT(LPXLOPER12, "Q");
+DEFINE_TYPE_TEXT(bool,       "A");
+DEFINE_TYPE_TEXT(bool*,      "L");
+DEFINE_TYPE_TEXT(double,     "B");
+DEFINE_TYPE_TEXT(double*,    "E");
+DEFINE_TYPE_TEXT(char*,      "C");
+DEFINE_TYPE_TEXT(uint16_t,   "H");
+DEFINE_TYPE_TEXT(int16_t,    "I");
+DEFINE_TYPE_TEXT(int16_t*,   "M");
+DEFINE_TYPE_TEXT(int32_t,    "J");
+DEFINE_TYPE_TEXT(int32_t*,   "N");
+DEFINE_TYPE_TEXT(wchar_t*,   "C%");
+DEFINE_TYPE_TEXT(LPCWSTR,    "C%");
+DEFINE_TYPE_TEXT(LPXLOPER12, "Q");
 
 struct FunctionInfo
 {
@@ -343,18 +350,12 @@ template <typename Func> class FunctionInfoFactory;
 template <typename TRet, typename... TArgs>
 struct FunctionInfoFactory<TRet(TArgs...)>
 {
-	template <typename T>
-	static LPCWSTR GetTypeText()
-	{
-		return GET_ARGUMENT_TYPE_TEXT(typename std::decay<T>::type);
-	}
-
 	static FunctionInfo Create(LPCWSTR entryPoint)
 	{
 		const int NumArgs = sizeof...(TArgs);
 		std::array<LPCWSTR, NumArgs + 1> texts = {
-			GetTypeText<typename UdfReturnValue<typename std::decay<TRet>::type>::boxed_type>(),
-			GetTypeText<typename UdfArgument<typename std::decay<TArgs>::type>::boxed_type>()...
+			GetTypeText<typename ReturnValueWrapper<TRet>::wrapped_type>(),
+			GetTypeText<typename ArgumentWrapper<TArgs>::wrapped_type>()...
 		};
 		std::wstring s;
 		for (int i = 0; i <= NumArgs; i++)
@@ -408,14 +409,14 @@ class ExcelException : public std::exception
 	}
 
 public:
-	ExcelException(int errorCode)
+	explicit ExcelException(int errorCode)
 		: m_errorCode(errorCode)
 	{
 		if (GetKnownErrorMessage(errorCode) == nullptr)
 			sprintf_s(m_errorMessage, "xll error %d", m_errorCode);
 	}
 
-	virtual const char* what() const
+	virtual const char* what() const override
 	{
 		const char *msg = GetKnownErrorMessage(m_errorCode);
 		return msg? msg : m_errorMessage;
@@ -442,15 +443,15 @@ public:
 // On 32-bit platform, we use naked function to emit a JMP instruction,
 // and export this function. The function must be __cdecl.
 
-template <typename Func, Func *func, typename> struct XLWrapper { };
+template <typename Func, Func *func, typename> struct XLWrapper;
 template <typename Func, Func *func, typename TRet, typename... TArgs>
 struct XLWrapper < Func, func, TRet(TArgs...) >
 { 
-	static typename UdfReturnValue<typename std::decay<TRet>::type>::boxed_type __stdcall
-		Call(typename UdfArgument<typename std::decay<TArgs>::type>::boxed_type... args)
+	static typename ReturnValueWrapper<TRet>::wrapped_type __stdcall
+		Call(typename ArgumentWrapper<TArgs>::wrapped_type... args)
 	{
-		return UdfReturnValue<typename std::decay<TRet>::type>::box(
-			func(UdfArgument<typename std::decay<TArgs>::type>::unbox(args)...));
+		return ReturnValueWrapper<TRet>::wrap(
+			func(ArgumentWrapper<TArgs>::unwrap(args)...));
 	}
 };
 
